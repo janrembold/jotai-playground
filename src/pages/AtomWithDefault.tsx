f22294455d99e9d9356ceef86b7c52bcca30ca8f
janrembold <janrembold@gmx.net>
2023-05-26 17:22:20 +0200

    remove unused code

--- src/pages/AtomWithDefault.tsx
+++ src/pages/AtomWithDefault.tsx
@@ -1,14 +1,8 @@
 import { Box } from "@mui/material";
 import { Markdown } from "../components/Markdown";
 import md from "../slides/atomWithDefault.md?raw";
-import { useAtom } from "jotai";
-import { atomWithDefault as a } from "jotai/utils";
-
-const bar = a(() => Date.now());
 
 export const AtomWithDefault = () => {
-  const foo = useAtom(bar);
-
   return (
     <Box>
       <Markdown text={md} />
